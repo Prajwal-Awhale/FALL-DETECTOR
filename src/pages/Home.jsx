223f4ffe0f3982cd58b6694cbdcb5da533ba9143
Prajwal-Awhale <Prajwalawhale01@gmail.com>
2025-03-02 19:26:16 +0530

    commit with login signup page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
-
+import { Link } from "react-router-dom";
 export default function Home() {
-    return (
-      <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
-        <h1 className="text-4xl font-bold text-blue-500">Welcome to Fall Detector</h1>
-        <p className="mt-4 text-lg text-gray-700">Your responsive React app is set up with Tailwind!</p>
+  return (
+    <>
+      <div>
+        <h1>THIS IS HOME PAGE</h1>
+        <h2>progress so far </h2>
+        <Link to="./login">LOGIN PAGE</Link>
       </div>
-    );
-  }
-  
+      <div className="text">
+        <Link to="./signup">Signup PAGE</Link>
+      </div>
+    </>
+  );
+}
